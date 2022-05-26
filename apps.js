be9ce43c75812ea8f0a544f792cf8cfdf2f698db
“GissellValdez” <Gissellvaldez@gmail.com>
2022-05-26 00:57:14 -0400

    js updated functionality to buttons and got the Player vs CPU mode to work

--- apps.js
+++ apps.js
@@ -12,6 +12,8 @@ const slots = document.getElementsByClassName('slot')
 //variables
 let currentPlayer = 1 // 1 for player 1, 2 for cpu/player2, starting with Player 1.
 let gameActive = true
+let playAgainstCpu = false
+let playAgainstPlayer2 = false
 
 //variables for stretch goal of having players pick their own colors
 //const player1Color = null
@@ -41,6 +43,9 @@ function startPlayerVsPc() {
 
 	//TO DO: add more logic for PvCPU.
 	//add the random selector
+
+    playAgainstCpu = true
+
 }
 
 function startPlayerVsPlayer() {
@@ -50,10 +55,21 @@ function startPlayerVsPlayer() {
 	playerVsPlayer.classList.add('hide')
 
 	//TO DO: add more logic for PvP
+    playAgainstPlayer2 = true
 }
 
 function startPlayAgain() {
 	//alert('Play Again!') //alert works
+	for (let i = 0; i < gameBoard.children.length; i++) {
+        if (gameBoard.children[i].classList == 'takenSlotByPlayer'){
+            gameBoard.children[i].classList.add('slot')
+            gameBoard.children[i].classList.remove('takenSlotByPlayer')
+        } else if (gameBoard.children[i].classList == 'takenSlotByOther'){
+            gameBoard.children[i].classList.add('slot')
+			gameBoard.children[i].classList.remove('takenSlotByOther')
+        }
+    }
+
 	playerVsPc.classList.remove('hide')
 	playerVsPlayer.classList.remove('hide')
 
@@ -65,7 +81,8 @@ function clickSlotSelector() {
 	for (let i = 0; i < slotSelector.length; i++) {
 		slotSelector[i].addEventListener('click', (e) => {
 			//alert('selector clicked') //alert works
-			completePlayerMoveAfterClickingSlotSelector(i)
+			completePlayerMoveAfterClickingSlotSelector(i);
+            makeCpuRandomTurn();
 		})
 	}
 }
@@ -105,14 +122,25 @@ function completePlayerMoveAfterClickingSlotSelector(slotSelectedColumn) {
 			}
 		}
 	}
+}
 
-	function changePlayerTurn() {
-		if (currentPlayer == 1) {
-			currentPlayer = 2
-		} else {
-			currentPlayer = 1
-		}
+//this function changes the player's turn from 1 to 2 and 2 to 1 whenever a player goes
+function changePlayerTurn() {
+	if (currentPlayer == 1) {
+		currentPlayer = 2
+	} else {
+		currentPlayer = 1
 	}
+}
+
+function makeCpuRandomTurn(){
+    if (playAgainstCpu) {
+        if(currentPlayer == 2){
+            let cpuTurnRandomColumn = Math.floor(Math.random() * 7);
+            completePlayerMoveAfterClickingSlotSelector(cpuTurnRandomColumn);
+            //changePlayerTurn();
+        }
+    }
 }
 
 main()
