cadf2f51e466ca62aebe3831cd82a24e3e8479af
“GissellValdez” <Gissellvaldez@gmail.com>
2022-05-26 02:22:17 -0400

    js updated some variable names and added more comments

--- apps.js
+++ apps.js
@@ -1,51 +1,53 @@
+//---------------------------- DOM ----------------------------//
+
 //get the buttons and board from the html
 const playerVsPc = document.getElementById('pvc')
 const playerVsPlayer = document.getElementById('pvp')
 const playAgain = document.getElementById('restart')
 const gameBoard = document.getElementById('board')
 const gameSlots = document.getElementsByClassName('slot')
-//get the top row of slots, aka the selector slots
+//get the top row of slots, aka the selector slots. these will be used to get the columns to make the player's move in
 const slotSelector = document.getElementsByClassName('slotSelector')
 //get the rest of the slots
 const slots = document.getElementsByClassName('slot')
 
-//variables
-let currentPlayer = 1 // 1 for player 1, 2 for cpu/player2, starting with Player 1.
+
+
+//---------------------------- VARIABLES ----------------------------//
+
+// 1 for player 1, 2 for cpu/player2, starting with Player 1.
+let currentPlayer = 1
 let gameActive = true
 let playAgainstCpu = false
 let playAgainstPlayer2 = false
-
 //variables for stretch goal of having players pick their own colors
 //const player1Color = null
 //const player2Color = null
 
-//listener for when the board is clicked. 
-function listeners() {
-	document
-		.querySelector('div.slotSelector')
-		.addEventListener('click', clickSlotSelector)
-}
 
+
+//---------------------------- FUNCTIONS ----------------------------//
+
+//main function that will be called to run the game
 function main() {
 	//these three lines are the listeners for when the buttons are clicked. they are linked the functions above
 	playerVsPc.onclick = startPlayerVsPc
 	playerVsPlayer.onclick = startPlayerVsPlayer
 	playAgain.onclick = startPlayAgain
+
+	//listener for the selector/column slots
 	clickSlotSelector()
-	//this is the listener for when the slotSelectors are clicked
-	//listeners();
 }
+
+
 function startPlayerVsPc() {
 	//alert("Player vs PC") //alert works
-	//these two lines hide the buttons so that the players don't accidentally click them
+	//these two lines hide the game mode buttons so that the players don't accidentally click them
 	playerVsPc.classList.add('hide')
 	playerVsPlayer.classList.add('hide')
 
-	//TO DO: add more logic for PvCPU.
-	//add the random selector
-
-    playAgainstCpu = true
-
+    //set the variable to true so that the CPU's turn can be automated in the main listener
+	playAgainstCpu = true
 }
 
 function startPlayerVsPlayer() {
@@ -55,67 +57,69 @@ function startPlayerVsPlayer() {
 	playerVsPlayer.classList.add('hide')
 
 	//TO DO: add more logic for PvP
-    playAgainstPlayer2 = true
+	playAgainstPlayer2 = true
 }
 
 function startPlayAgain() {
 	//alert('Play Again!') //alert works
+    // this for loop goes through all of the slots and if they are taken, they are released/reset for the next game
+    //returning to class slot will make the slots white, which is the color used as "empty"
 	for (let i = 0; i < gameBoard.children.length; i++) {
-        if (gameBoard.children[i].classList == 'takenSlotByPlayer'){
-            gameBoard.children[i].classList.add('slot')
-            gameBoard.children[i].classList.remove('takenSlotByPlayer')
-        } else if (gameBoard.children[i].classList == 'takenSlotByOther'){
-            gameBoard.children[i].classList.add('slot')
+		if (gameBoard.children[i].classList == 'takenSlotByPlayer') {
+			gameBoard.children[i].classList.add('slot')
+			gameBoard.children[i].classList.remove('takenSlotByPlayer')
+		} else if (gameBoard.children[i].classList == 'takenSlotByOther') {
+			gameBoard.children[i].classList.add('slot')
 			gameBoard.children[i].classList.remove('takenSlotByOther')
-        }
-    }
+		}
+	}
 
+    //the following two lines remove the the hide class from the player mode buttons so that they can show on the screen
 	playerVsPc.classList.remove('hide')
 	playerVsPlayer.classList.remove('hide')
 
-	//TO DO: add more logic for clearing the board
+    // reset the first player to be player 1
+    currentPlayer = 1
+
+    // reset the CPU game mode to false
+    playAgainstCpu = false
 }
 
 //this is the function for when a slotSelector is chosen
 function clickSlotSelector() {
+    // for loop to add an event listener to the selector slots 
+    // when clicked, runs the functions to perform player/cpu turns
 	for (let i = 0; i < slotSelector.length; i++) {
 		slotSelector[i].addEventListener('click', (e) => {
 			//alert('selector clicked') //alert works
-			completePlayerMoveAfterClickingSlotSelector(i);
-            makeCpuRandomTurn();
+			completePlayerMoveAfterSelectingSlot(i)
+			makeCpuRandomTurn()
 		})
 	}
 }
 
-function completePlayerMoveAfterClickingSlotSelector(slotSelectedColumn) {
-	for (let i = 42; i >= 0; i = i - 7) {
-		let bottomSlotOfCurrentSelectedColumn = slotSelectedColumn + i
+function completePlayerMoveAfterSelectingSlot(slotSelectedColumn) {
+	//loop through all of the slots on the board. selector slots are treated as a row, so the logic is setup accordingly to not fill them
+    for (let i = 42; i >= 0; i = i - 7) {
+        // save the most bottom row in variable to be used in conditionals to check if that row is empty
+		let bottomRowOfColumn = slotSelectedColumn + i
+        //first conditional checks for whose turn it is (player 1 or cpu/player2)
 		if (currentPlayer == 1) {
-			if (
-				gameBoard.children[bottomSlotOfCurrentSelectedColumn].classList ==
-				'slot'
-			) {
-				gameBoard.children[bottomSlotOfCurrentSelectedColumn].classList.add(
-					'takenSlotByPlayer'
-				)
-				gameBoard.children[bottomSlotOfCurrentSelectedColumn].classList.remove(
-					'slot'
-				)
+            //if the row is empty and it it player one's turn, the slot is updated with player1's color
+            //this is done by removing the slot class and adding takenSlotByPlayer class
+			if (gameBoard.children[bottomRowOfColumn].classList == 'slot') {
+				gameBoard.children[bottomRowOfColumn].classList.add('takenSlotByPlayer')
+				gameBoard.children[bottomRowOfColumn].classList.remove('slot')
 				changePlayerTurn()
 				//alert(`It works, Coordinated ${slotSelectedColumn},${bottomSlotOfCurrentSelectedColumn}, ${gameBoard.children[bottomSlotOfCurrentSelectedColumn].classlist}`)
 				break
 			}
 		} else {
-			if (
-				gameBoard.children[bottomSlotOfCurrentSelectedColumn].classList ==
-				'slot'
-			) {
-				gameBoard.children[bottomSlotOfCurrentSelectedColumn].classList.add(
-					'takenSlotByOther'
-				)
-				gameBoard.children[bottomSlotOfCurrentSelectedColumn].classList.remove(
-					'slot'
-				)
+            //if the row is empty and it it player one's turn, the slot is updated with cpu/player2's color
+            //this is done by removing the slot class and adding takenSlotByPlayer class
+			if (gameBoard.children[bottomRowOfColumn].classList =='slot') {
+				gameBoard.children[bottomRowOfColumn].classList.add('takenSlotByOther')
+				gameBoard.children[bottomRowOfColumn].classList.remove('slot')
 				changePlayerTurn()
 				//alert(`It works, Coordinated ${slotSelectedColumn},${bottomSlotOfCurrentSelectedColumn}, ${gameBoard.children[bottomSlotOfCurrentSelectedColumn].classlist}`)
 				break
@@ -127,20 +131,28 @@ function completePlayerMoveAfterClickingSlotSelector(slotSelectedColumn) {
 //this function changes the player's turn from 1 to 2 and 2 to 1 whenever a player goes
 function changePlayerTurn() {
 	if (currentPlayer == 1) {
+		//if current turn belongs to Player 1, update the variable to change to Player2/CPU
 		currentPlayer = 2
 	} else {
+		//if not set to player 1, updates the variable so that it is
 		currentPlayer = 1
 	}
 }
 
-function makeCpuRandomTurn(){
-    if (playAgainstCpu) {
-        if(currentPlayer == 2){
-            let cpuTurnRandomColumn = Math.floor(Math.random() * 7);
-            completePlayerMoveAfterClickingSlotSelector(cpuTurnRandomColumn);
-            //changePlayerTurn();
-        }
-    }
+//this function does the CPU's random move
+function makeCpuRandomTurn() {
+	//check is the player selected to play against CPU
+	if (playAgainstCpu) {
+		//if yes, goes through another conditional to check if it is CPU/Player2 turn
+		if (currentPlayer == 2) {
+			//if yes to above two conditionals, generate a number between 0 and the length of slotSelector, which is 7
+			//this is dont to get the column for the CPU to play in
+			//slotSelector.length was hardcoded in previous commit.
+			let cpuTurnRandomColumn = Math.floor(Math.random() * slotSelector.length)
+			//calls the function to perform the CPU's move with the random column generated as a parameter
+			completePlayerMoveAfterSelectingSlot(cpuTurnRandomColumn)
+		}
+	}
 }
 
 main()
